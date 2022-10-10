808f04af48168e798b8e4a91a93df420c291dfd3
Eznamen <111395043+Eznamen@users.noreply.github.com>
2022-10-11 02:23:51 +0300

    cleaning in code

--- src/Main.java
+++ src/Main.java
@@ -14,8 +14,6 @@ public class Main {
         people.add(new Person("Katya", "Mamina-Naumova-Ivanova", 27));
         System.out.println(people);
 
-//        PersonNameAmountWordsComparator personNameAmountWordsComparator = new PersonNameAmountWordsComparator();
-//        personNameAmountWordsComparator.setAmountWordSurname(3);
 
         Comparator<Person> comparator = (p1, p2) -> {
             int comp = Integer.compare(p2.getSurname().length, p1.getSurname().length);
@@ -33,39 +31,9 @@ public class Main {
             return result;
         };
 
-
         Collections.sort(people, comparator);
         System.out.println(people);
 
     }
-    
+
 }
-
-
-//public class PersonNameAmountWordsComparator implements Comparator<Person> {
-//    private int amuntWordSurnameuntWordSurname;
-//
-//    public void setAmountWordSurname(int amountWordSurname) {
-//        this.amuntWordSurnameuntWordSurname = amountWordSurname;
-//    }
-//
-//    public int getAmuntWordSurnameuntWordSurname() {
-//
-//        return amuntWordSurnameuntWordSurname;
-//    }
-//
-//    @Override
-//    public int compare(Person p1, Person p2) {
-//        int comparater = Integer.compare(p2.getSurname().length, p1.getSurname().length);
-//        if (comparater != 0) {
-//            if (p1.getSurname().length > getAmuntWordSurnameuntWordSurname() && p2.getSurname().length > getAmuntWordSurnameuntWordSurname()) {
-//                return Integer.compare(p2.getAge(), p1.getAge());
-//            } else {
-//                return comparater;
-//            }
-//        } else {
-//            return Integer.compare(p2.getAge(), p1.getAge());
-//        }
-//
-//
-//    }
