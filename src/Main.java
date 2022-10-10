0c086a01dd20cc5afd2e147b82ea640c3ee421f8
Eznamen <111395043+Eznamen@users.noreply.github.com>
2022-10-11 02:20:13 +0300

    смена условия на lambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -13,11 +14,58 @@ public class Main {
         people.add(new Person("Katya", "Mamina-Naumova-Ivanova", 27));
         System.out.println(people);
 
-        PersonNameAmountWordsComparator personNameAmountWordsComparator = new PersonNameAmountWordsComparator();
-        personNameAmountWordsComparator.setAmountWordSurname(3);
+//        PersonNameAmountWordsComparator personNameAmountWordsComparator = new PersonNameAmountWordsComparator();
+//        personNameAmountWordsComparator.setAmountWordSurname(3);
 
-        Collections.sort(people, personNameAmountWordsComparator);
+        Comparator<Person> comparator = (p1, p2) -> {
+            int comp = Integer.compare(p2.getSurname().length, p1.getSurname().length);
+            int result;
+            int maxWord = 3;
+            if (comp != 0) {
+                if (p1.getSurname().length > maxWord && p2.getSurname().length > maxWord) {
+                    result = Integer.compare(p2.getAge(), p1.getAge());
+                } else {
+                    result = comp;
+                }
+            } else {
+                result = Integer.compare(p2.getAge(), p1.getAge());
+            }
+            return result;
+        };
+
+
+        Collections.sort(people, comparator);
         System.out.println(people);
 
     }
+    
 }
+
+
+//public class PersonNameAmountWordsComparator implements Comparator<Person> {
+//    private int amuntWordSurnameuntWordSurname;
+//
+//    public void setAmountWordSurname(int amountWordSurname) {
+//        this.amuntWordSurnameuntWordSurname = amountWordSurname;
+//    }
+//
+//    public int getAmuntWordSurnameuntWordSurname() {
+//
+//        return amuntWordSurnameuntWordSurname;
+//    }
+//
+//    @Override
+//    public int compare(Person p1, Person p2) {
+//        int comparater = Integer.compare(p2.getSurname().length, p1.getSurname().length);
+//        if (comparater != 0) {
+//            if (p1.getSurname().length > getAmuntWordSurnameuntWordSurname() && p2.getSurname().length > getAmuntWordSurnameuntWordSurname()) {
+//                return Integer.compare(p2.getAge(), p1.getAge());
+//            } else {
+//                return comparater;
+//            }
+//        } else {
+//            return Integer.compare(p2.getAge(), p1.getAge());
+//        }
+//
+//
+//    }
