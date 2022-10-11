889a63d2f7249fc783a141e14c79b4fb51e86044
Eznamen <111395043+Eznamen@users.noreply.github.com>
2022-10-11 21:12:36 +0300

    упрощение лямбды

--- src/Main.java
+++ src/Main.java
@@ -12,28 +12,19 @@ public class Main {
         people.add(new Person("Katya", "Mamina-Naumova-Ivanova-iiii", 45));
         people.add(new Person("Katya", "Mamina Naumova Ivanova iii ooo", 30));
         people.add(new Person("Katya", "Mamina-Naumova-Ivanova", 27));
-        System.out.println(people);
 
+        System.out.println(people);
 
         Comparator<Person> comparator = (p1, p2) -> {
-            int comp = Integer.compare(p2.getSurname().length, p1.getSurname().length);
-            int result;
             int maxWord = 3;
-            if (comp != 0) {
-                if (p1.getSurname().length > maxWord && p2.getSurname().length > maxWord) {
-                    result = Integer.compare(p2.getAge(), p1.getAge());
-                } else {
-                    result = comp;
-                }
+            if (p1.getSurname().length > maxWord && p2.getSurname().length > maxWord) {
+                return p2.getAge() - p1.getAge();
             } else {
-                result = Integer.compare(p2.getAge(), p1.getAge());
+                return p2.getSurname().length - p1.getSurname().length;
             }
-            return result;
         };
 
-        Collections.sort(people, comparator);
+        people.sort(comparator);
         System.out.println(people);
-
     }
-
 }
